28163def43945569cd5375e576e7cfaced88bb7c
buyath <buyaththayub@gmail.com>
2026-06-13 17:37:49 +0000

    feat :added  count todo and dark and light theme functionality

--- frontend-projects/Todo-app/script.js
+++ frontend-projects/Todo-app/script.js
@@ -1,13 +1,26 @@
 const userInput = document.querySelector("#userinput");
 const  addButton = document.getElementById("add-btn");
 const todoItemContainer = document.querySelector('.todo-items-container');
+const todoCountText = document.querySelector('.todo-count');
+const themeButton = document.querySelector('.theme-button');
+const bodyColor = document.querySelector('.body');
+let todoCount = 0;
 
+document.addEventListener("DOMContentLoaded",() => {
+    console.log("the page loaded fully");
+    todoCountText.textContent = `todo count is : ${todoCount}`; 
+})
 
-
-
+function validateInput(input){
+    if(input == ""){
+        alert("the input is empty");
+        return false;
+    }    
+    return true;
+}
 
 function  createTodo(text){
-    const container = document.createElement("div");
+    const  container = document.createElement("div");
     container.classList.add("todo-item");
     
     const todoItem = document.createElement("p");
@@ -21,11 +34,13 @@ function  createTodo(text){
     container.appendChild(todoItem);
     container.appendChild(deleteButton);
     todoItemContainer.appendChild(container);
-    
+    todoCount++;
+
+    todoCountText.textContent =`todo count is : ${todoCount}`; 
 
     todoItem.addEventListener("click",() => {
         if(todoItem.classList.contains("done")){
-            todoItem.classList.remove("done");
+            todoItem.classLilsst.remove("done");
         }
         else{
             todoItem.classList.add("done");  
@@ -34,10 +49,9 @@ function  createTodo(text){
     
     deleteButton.addEventListener("click",(e) => {
         e.target.parentElement.remove();
+        todoCount--;
+        todoCountText.textContent = `todo count is : ${todoCount}`; 
     });
-
-    
-
 }
 
 function clearInput(){
@@ -45,8 +59,22 @@ function clearInput(){
 }
 
 addButton.addEventListener("click",() => {
-    createTodo(userInput.value);
-    clearInput();
+    if(validateInput(userInput.value)){
+        createTodo(userInput.value);
+        clearInput();   
+    }
 });
 
 
+
+
+themeButton.addEventListener("click",() => {
+    if(bodyColor.style.backgroundColor == "white"){
+        bodyColor.style.backgroundColor = "black"; 
+        todoCountText.style.color = "white";     
+    }
+    else{
+        bodyColor.style.backgroundColor = "white";
+        todoCountText.style.color = "black";
+    }
+})
