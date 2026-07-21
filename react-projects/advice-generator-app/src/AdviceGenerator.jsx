5a998e1b717f1f5ca8eb806f6bbc14f22817821a
buyath <buyaththayub@gmail.com>
2026-07-21 17:52:03 +0000

    feat: completed advice generator app

--- react-projects/advice-generator-app/src/AdviceGenerator.jsx
+++ react-projects/advice-generator-app/src/AdviceGenerator.jsx
@@ -1,20 +1,32 @@
 import { useState,useEffect } from "react";
+import pattern_divider from "./images/pattern-divider-desktop.svg";
+import dice_button from "./images/icon-dice.svg";
 const AdviceGenerator = () => {
+    const [advice,setAdvice] = useState({})
     async function fetchAdvice(){
         try{
             const response = await fetch("https://api.adviceslip.com/advice")
             const advice = await response.json();
-            console.log(advice);
+            setAdvice(advice.slip);
         }
         catch(err){
             console.log(err);
         }
     }
-    fetchAdvice();
+    useEffect(() => {        
+        fetchAdvice();
+    },[]);
     return (
         <div className="advice-container">
             <div className="container">
-                <p>ADVICE 1223</p>
+                <p>ADVICE {advice.id}</p>
+                <h2>{advice.advice}</h2>
+                <div className="img-container">
+                    <img src={pattern_divider} alt="play button" />
+                </div>
+                <div className="button-container">
+                    <img src={dice_button} alt="dice-button" onClick={fetchAdvice}/>
+                </div>
             </div>
         </div>
     )
