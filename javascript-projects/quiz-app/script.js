269bc3bb794685a747a5201c6a3acd3520a5dcb9
buyath <buyaththayub@gmail.com>
2026-07-24 19:03:50 +0000

    style: complete quiz app UI

--- javascript-projects/quiz-app/script.js
+++ javascript-projects/quiz-app/script.js
@@ -5,19 +5,29 @@ const quizOptionContainer = document.querySelector(".quiz-option-container");
 const nextBtn = document.getElementById("next-btn");
 
 let index = 0;
-console.log(quizData); 
-function createQuiz(){
+
+function createQuiz(){    
+    quizOptionContainer.innerHTML = "";
     quizQuestion.textContent = quizData[index].question;
     quizData[index].options.forEach((option) => {
         let optionText = document.createElement('p');
         optionText.textContent = option;
         quizOptionContainer.appendChild(optionText);
+
+       
     });
 }
 
 nextBtn.addEventListener("click", () => {
     index++;
-    createQuiz()
-})
+    if(index > quizData.length){
+        index = 0;
+    }
+    if(index < quizData.length){
+        createQuiz();
+    } 
+});
 
-// createQuiz();
+window.addEventListener("DOMContentLoaded", () => {
+    createQuiz();
+})
