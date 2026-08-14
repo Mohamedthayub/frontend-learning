769f7d44ec08d3361cf0db505f23a45f08dc17bb
buyath <buyaththayub@gmail.com>
2026-08-14 18:31:22 +0000

    style : completed in github-user-finder

--- react-projects/github-user-finder/src/components/Card.jsx
+++ react-projects/github-user-finder/src/components/Card.jsx
@@ -1,4 +1,9 @@
 import { useState } from "react";
+import thayub_profile from "../assets/thayub.jpg";
+import twitter_icon from "../assets/icon-twitter.svg";
+import location_icon from "../assets/icon-location.svg";
+import website_icon  from "../assets/icon-website.svg";
+import company_icon from "../assets/icon-company.svg";
 const SearchInput = () => {
     const [username,setUsername]  = useState("");
 
@@ -11,47 +16,50 @@ const SearchInput = () => {
             <div className="card">
                 <div className="first">
                     <div className="first-container">
-                    <img src="" alt="" />
+                    <div>
+                        <img src="../assets/icon-search.svg" alt="" />
+                        <img src={thayub_profile}alt=""  className="profile-photo"/>
+                    </div>
                     </div>
                    <div className="second-container">
-                    <h2>The Octocat</h2>
-                    <h3>@octocat</h3>
-                    <p>this profile has no bio</p>
+                    <h2>Mohideen thayub</h2>
+                    <h3>@Mohideenthayub</h3>
+                    <p className="bio">this profile has no bio</p>
                    </div>
                 </div>
-                <div className="second-contain">
-                    <div>
+                <div className="second-container">
+                    <div className="small-container1">
                         <h3>Public Repositories</h3>
-                        <p>4</p>
+                        <p className="text">4</p>
                     </div>
-                    <div>
+                    <div className="small-container2">
                         <h3>Followers</h3>
-                        <p>5</p>
+                        <p className="text">5</p>
                     </div>
-                    <div>
+                    <div className="small-container3">
                         <h3>following</h3>
-                        <p>2</p>
+                        <p className="text">2</p>
                     </div>
                 </div>
                 <div className="third">
-                    <div>
-                        <div>
-                            <img src="../assets/icon-location.svg" alt="" />
-                            <p>Not available</p>
+                    <div className="first-third-container">
+                        <div className="third-small-container">
+                            <img src={location_icon}  alt="" className='location-img'/>
+                            <p className="third-text">Not available</p>
                         </div>
-                        <div>
-                            <img src="../assets/icon-twitter.svg" alt="" />
-                            <p>Not available</p>
+                        <div className="third-small-container">
+                            <img src={twitter_icon} alt="" className="twitter-img"/>
+                            <p className="third-text">Not available</p>
                         </div>
                     </div>
-                    <div>
-                        <div>
-                            <img src="../assets/icon-website.svg" alt="" />
-                            <p>Not available</p>
+                    <div className="second-third-container">
+                        <div className="third-small-container">
+                            <img src={website_icon} alt="" className="website-img" />
+                            <p className="third-text">Not available</p>
                         </div>
-                        <div>
-                            <img src="../assets/icon-company.svg" alt="" />
-                            <p>Not available</p>
+                        <div className="third-small-container">
+                            <img src={company_icon} alt="" className="company-img" />
+                            <p className="third-text">Not available</p>
                         </div>
                     </div>
                 </div>
