c94d71133465a928a578601a1a26d63f2b68ad85
buyath <buyaththayub@gmail.com>
2026-08-25 16:49:35 +0000

    completed vs code layout

--- test/react-routing-test/src/components/Navbar.jsx
+++ test/react-routing-test/src/components/Navbar.jsx
@@ -1,13 +1,13 @@
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 const Navbar  = () => {
     return (
         <div className="navbar">
             <h2>Navbar</h2>
             <ul>
-                <Link to='/'><li>Home</li></Link>
-                <Link to='/about'><li>About</li></Link>
-                <Link to='/products'><li>Products</li></Link>
-                <Link to='/contact'><li>Contact</li></Link>
+                <NavLink to='/'><li>Home</li></NavLink>
+                <NavLink to='/about'><li>About</li></NavLink>
+                <NavLink to='/products'><li>Products</li></NavLink>
+                <NavLink to='/contact'><li>Contact</li></NavLink>
             </ul>
             <button className="get-start-button">
                 Get Start
