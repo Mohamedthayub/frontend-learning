e1f194052c1f9949316deb6abbab0a6b0f068107
buyath <buyaththayub@gmail.com>
2026-08-25 16:06:26 +0000

    revition completed with createBrowser in react routing

--- test/react-routing-test/src/App.jsx
+++ test/react-routing-test/src/App.jsx
@@ -1,23 +1,20 @@
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Products from "./pages/Products";
 import About from "./pages/About";
-import {Routes,Route} from "react-router-dom";
+import {Route,createBrowserRouter,createRoutesFromElements,RouterProvider} from "react-router-dom";
+import RootLayout from "./layout/RootLayout";
 const App = () => {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element = {<RootLayout/>}>
+      <Route index element = {<Home/>}/>
+      <Route path ="about" element = {<About/>}/>
+      <Route path="products" element = {<Products/>}/>
+      <Route path="contact" element = {<Contact/>}/>
+    </Route>
+  ))
   return (
-    <div>
-      <Navbar/>
-      <div className="container">
-        <Routes>
-          <Route path="/" element= {<Home/>}/>
-          <Route path="/about" element = {<About/>}/>
-          <Route path="/products" element ={<Products/>}/>
-          <Route path="/contact" element = {<Contact/>}/>
-        </Routes>
-      </div>
-    </div>
+    <RouterProvider router = {router}/>
   )
-
 }
 export default App;
--- test/react-routing-test/src/components/Navbar.jsx
+++ test/react-routing-test/src/components/Navbar.jsx
@@ -1,17 +1,17 @@
 import {Link} from "react-router-dom";
-const Navbar = () => {
+const Navbar  = () => {
     return (
         <div className="navbar">
-            <h1>Navbar</h1>
+            <h2>Navbar</h2>
             <ul>
                 <Link to='/'><li>Home</li></Link>
                 <Link to='/about'><li>About</li></Link>
                 <Link to='/products'><li>Products</li></Link>
                 <Link to='/contact'><li>Contact</li></Link>
             </ul>
-            <div className="btn-container">
-                <button className="get-start-button">Get Start</button>
-            </div>
+            <button className="get-start-button">
+                Get Start
+            </button>
         </div>
     )
 }
