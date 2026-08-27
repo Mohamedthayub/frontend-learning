d4ae5bcbde02c1c679762a08eeeb394770058709
buyath <buyaththayub@gmail.com>
2026-08-27 18:57:15 +0000

    revised new  createBrowser routing

--- react-routing-revise/src/components/Navbar.jsx
+++ react-routing-revise/src/components/Navbar.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 const Navbar  = ()  => {
     return (
         <div className="navbar">
             <h1>Navbar</h1>
             <ul>
-                <Link to='/'><li>Home</li></Link>
-                <Link to='/about'><li>About</li></Link>
-                <Link to='/products'><li>Products</li></Link>
-                <Link to='/contact'><li>Contact</li></Link>
+                <NavLink to='/'><li>Home</li></NavLink>
+                <NavLink to='/about'><li>About</li></NavLink>
+                <NavLink to='/products'><li>Products</li></NavLink>
+                <NavLink to='/contact'><li>Contact</li></NavLink>
             </ul>
             <div className="btn-container">
                 <button className="get-start-button">Get Started</button>
