a4f36ba2a7ae193e156c079841ef6a373e20eb65
buyath <buyaththayub@gmail.com>
2026-08-27 18:44:39 +0000

    revised browser router revition

---
+++ react-routing-revise/src/App.jsx
@@ -0,0 +1,22 @@
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+import {Routes,Route} from "react-router-dom";
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <div className="container"> 
+        <Routes>
+          <Route path="/" element = {<Home/>}/>
+          <Route path="/about" element = {<About/>}/>
+          <Route path="/products" element = {<Products/>}/>
+          <Route path="/contact" element = {<Contact/>}/>
+        </Routes>
+      </div>
+    </div>
+  )
+}
+export default App;
---
+++ react-routing-revise/src/pages/Products.jsx
@@ -0,0 +1,8 @@
+const Products  = () => {
+    return (
+        <div>
+            <h1 className="heading">Products Page</h1>
+        </div>
+    )
+}
+export default Products;
